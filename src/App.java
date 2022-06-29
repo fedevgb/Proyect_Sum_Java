524fa0b01603587c0ab0e6cd137259ed9f8b8bbe
fedevgb <federicopetrello@gmail.com>
2022-06-29 02:00:16 -0300

    Actualilzacion Funcion Suma

--- src/App.java
+++ src/App.java
@@ -1,15 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int var1 = 10;
-        int var2 = 40;
-        int var3 = 60;
         
-        
+        //llamamos a la funcion suma
+        suma();
       
     }
 
-    public static void suma(int var1, int var2, int var3){
-
+    public static void suma(){
+        int var1 = 10;
+        int var2 = 40;
+        int var3 = 60;
         int resultado;
 
         resultado = var1 + var2 + var3;
