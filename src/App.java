f38ab1478ca48b434c19459c37b973ccc207aa63
fedevgb <federicopetrello@gmail.com>
2022-06-29 01:41:16 -0300

    Primera Parte

---
+++ src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int var1 = 10;
+        int var2 = 40;
+        int var3 = 60;
+        
+        
+      
+    }
+
+    public static void suma(int var1, int var2, int var3){
+
+        int resultado;
+
+        resultado = var1 + var2 + var3;
+
+        System.out.println("El resultado de la suma es: " + resultado);
+        
+    }   
+
+}
